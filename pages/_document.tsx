2ddbaa009a4c13cccea26e400bf6ee84d60ca536
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-12-26 01:57:16 +0900

    fix: modify Website icon logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
 				<meta charSet="utf-8" />
 				<meta name="robots" content="index,follow" />
 
-				<link rel="icon" href="/img/logo/favicon.svg" />
+				<link rel="icon" href="/img/icons/main-logotip.svg" />
 				<link rel="apple-touch-icon" href="/img/logo/apple-touch-icon.png" sizes="180x180" />
 			</Head>
 			<body>
