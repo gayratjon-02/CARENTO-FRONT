2b652aa87743d6f8f8f1ef3d7a71373481f622a4
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-12-27 00:22:05 +0900

    fix: modify error for SERVER

--- libs/types/booking/booking.update.ts
+++ libs/types/booking/booking.update.ts
@@ -1,19 +1,7 @@
-import { Field, InputType } from "@nestjs/graphql";
-import { ObjectId } from "mongoose";
-import { IsNotEmpty, IsOptional } from "class-validator";
-import { BookingStatus, PaymentStatus } from "../../enums/booking.enum";
+import { BookingStatus, PaymentStatus } from 'libs/enum/booking.enum';
 
-@InputType()
-export class BookingUpdate {
-	@IsNotEmpty()
-	@Field(() => String)
+export interface BookingUpdate {
 	_id: string;
-
-	@IsOptional()
-	@Field(() => BookingStatus, { nullable: true })
 	bookingStatus?: BookingStatus;
-
-	@IsOptional()
-	@Field(() => PaymentStatus, { nullable: true })
 	paymentStatus?: PaymentStatus;
-}       
+}
