a65887de03ba495be955fab851e393d2972b167c
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-12-27 00:16:35 +0900

    fix: modify refactoring

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -4,10 +4,6 @@ import Head from 'next/head';
 import Top from '../Top';
 import Footer from '../Footer';
 import { Stack } from '@mui/material';
-import FiberContainer from '../common/FiberContainer';
-import HeaderFilter from '../homepage/HeaderFilter';
-import VideoCarousel from '../common/VideoCarousel';
-import HeroSection from '../homepage/HeroSection';
 import { userVar } from '../../../apollo/store';
 import { useReactiveVar } from '@apollo/client';
 import { getJwtToken, updateUserInfo } from '../../auth';
@@ -63,11 +59,7 @@ const withLayoutMain = (Component: any) => {
 							<Top />
 						</Stack>
 
-						<Stack className={'header-main'}>
-							<VideoCarousel />
-							<HeroSection />
-							{/* <FiberContainer /> */}
-						</Stack>
+						<Stack className={'header-main'}></Stack>
 
 						<Stack id={'main'}>
 							<Component {...props} />
